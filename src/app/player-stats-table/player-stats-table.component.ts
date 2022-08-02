54d90b0f457ac2a761c2c911b2e7c46232db4248
RadVonSwag <drewstes1@gmail.com>
2022-08-02 03:42:02 -0500

    setup workspace and generated table

---
+++ src/app/player-stats-table/player-stats-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { PlayerStatsTableDataSource, PlayerStatsTableItem } from './player-stats-table-datasource';
+
+@Component({
+  selector: 'app-player-stats-table',
+  templateUrl: './player-stats-table.component.html',
+  styleUrls: ['./player-stats-table.component.css']
+})
+export class PlayerStatsTableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<PlayerStatsTableItem>;
+  dataSource: PlayerStatsTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new PlayerStatsTableDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
